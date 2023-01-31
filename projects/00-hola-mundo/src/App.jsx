51c0aa2b04a8a66363bafa613f7a9982fb29b1f8
Fernando <fermg299@outlook.com>
2023-01-31 13:09:29 -0300

    Tarjeta twitter practicas

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+const users = [
+    {
+        username: 'FernandoMG81',
+        name: 'Fernando Martin Gordillo',
+        isFollowing: true
+    },
+    {
+        username: 'midudev',
+        name: 'Miguel Ángel Durán',
+        isFollowing: false
+    },
+    {
+        username: 'chesterlp40',
+        name: 'Ezequiel Rasgido',
+        isFollowing: false
+    },
+]
+
+
+
+export function App () {
+
+    return(
+        <section className='App'>
+            {
+                users.map(({ username, name, isFollowing}) => {
+                        return (
+                        <TwitterFollowCard
+                        key={username} 
+                        initialIsfollowing={isFollowing}
+                        username={username}>
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
+        </section>
+    )
 }
-
-export default App
