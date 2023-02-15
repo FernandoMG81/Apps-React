87366e6f3d2563bc3ebecf8900862e1e813e0be6
Fernando Gordillo <fermg299@outlook.com>
2023-02-15 16:20:15 -0300

    test conexion a mongodb

--- projects/05-node-api-backend/mongo.js
+++ projects/05-node-api-backend/mongo.js
@@ -1,14 +1,30 @@
 const mongoose = require('mongoose')
 
-const connectionString = process.env.MONGO_DB_URI
+const { MONGO_DB_URI, MONGO_DB_URI_TEST, NODE_ENV } = process.env
+
+const connectionString = NODE_ENV === 'test'
+  ? MONGO_DB_URI_TEST
+  : MONGO_DB_URI
 
 // Conexion a mongodb
 mongoose.set('strictQuery', true)
-mongoose.connect(connectionString)
-  .then(() => {
-    console.log('Database connected')
-  }).then(() => {
+// mongoose.connect(connectionString)
+//   .then(() => {
+//     console.log('Database connected')
+//   }).then(() => {
 
-  }).catch(err => {
-    console.error(err)
-  })
+//   }).catch(err => {
+//     console.error(err)
+//   })
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(connectionString)
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
