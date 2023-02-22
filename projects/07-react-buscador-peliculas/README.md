862b76347741d0ef698a00a0d5a841d7f3dd51c9
Fernando Gordillo <fermg299@outlook.com>
2023-02-22 11:11:12 -0300

    conexion a la API de peliculas, listado.

---
+++ projects/07-react-buscador-peliculas/README.md
@@ -0,0 +1,20 @@
+##Enunciado
+
+Crea una aplicacion para buscar peliculas
+
+API a usar:
+
+- https://www.omdbapi.com/
+- API_KEY: 9bc756e7
+
+Requerimientos:
+
+✅- Necesita mostrar un input para buscar la pelicula y un boton para buscar.
+✅ Lista las peliculas encontradas y muestra el titulo, año y poster.
+- Haz que las peliculas se muestren en un grid responsive.
+
+Primera iteracion:
+
+- Evitar que se haga la misma busqueda dos veces seguidas.
+- Haz que la busqueda se haga automaticamente al escribir.
+- Evita que se haga la busqueda continuamente al escribir (debounce)
---
+++ projects/07-react-buscador-peliculas/src/App.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useRef, useState } from 'react'
+import './App.css'
+import { Movies } from './components/Movies.jsx'
+import { useMovies } from './hooks/useMovies.js'
+
+function useSearch () {
+  const [search, setSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una pelicula vacia')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una pelicula con un numero')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La busqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, setSearch, error }
+}
+
+function App () {
+  const { search, setSearch, error } = useSearch()
+  const { movies, getMovies, loading } = useMovies({ search })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getMovies()
+  }
+
+  const handleChange = (event) => {
+    const newQuery = event.target.value
+    if (newQuery.startsWith(' ')) { return }
+    setSearch(newQuery)
+  }
+
+  return (
+    <div className='page'>
+      <header>
+        <h1>Buscador de películas</h1>
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }}
+            onChange={handleChange}
+            name='search'
+            value={search}
+            placeholder='star wars, avengers, The Matrix...'
+          />
+          <button type='submit'>Buscar</button>
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </header>
+
+      <main>
+        {loading ? <p>Buscando...</p> : <Movies movies={movies} />}
+
+      </main>
+    </div>
+  )
+}
+
+export default App
